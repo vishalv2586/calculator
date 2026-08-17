5bc049c98e31ad2a05b7fc98fef95463de566f39
vishalv2586 <vishalv2586@gmail.com>
2026-08-17 22:49:40 +0530

    Create cmodel.java

---
+++ cmodel.java
@@ -0,0 +1,16 @@
+public class CalculatorModel {
+    public double add(double a, double b) {
+        return a + b;
+    }
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+    public double divide(double a, double b) {
+        if (b == 0)
+            return 0;
+        return a / b;
+    }
+}
